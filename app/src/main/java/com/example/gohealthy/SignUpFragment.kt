121d05e1fe57bc00fd3053cfe1e1962e71a8664b
Eyaadd <eyadyehia07@gmail.com>
2024-09-12 01:53:39 +0300

    SignUp Fragment Added with its binding

---
+++ app/src/main/java/com/example/gohealthy/SignUpFragment.kt
@@ -0,0 +1,43 @@
+package com.example.gohealthy
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import com.example.gohealthy.databinding.FragmentSignupBinding
+
+
+class SignUpFragment : Fragment() {
+    lateinit var binding: FragmentSignupBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSignupBinding.inflate(inflater, container, false)
+
+        val gender = resources.getStringArray(R.array.Gender)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdownmenu, gender)
+
+
+        binding.genderTextField.editText?.let { autoCompleteTextView ->
+            (autoCompleteTextView as AutoCompleteTextView).setAdapter(arrayAdapter)
+        }
+
+
+
+
+   
+        return binding.root
+    }
+
+}
