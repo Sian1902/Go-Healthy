052917263419e3502df35adddc38120a89f9a049
Ahmed Haitham <haithamelsersy5060@gmail.com>
2024-09-12 21:23:41 +0300

    redesigned signup page

--- app/src/main/java/com/example/gohealthy/WelcomeFragment.kt
+++ app/src/main/java/com/example/gohealthy/WelcomeFragment.kt
@@ -5,22 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.gohealthy.databinding.FragmentSignupBinding
+import com.example.gohealthy.databinding.FragmentWelcomeBinding
 
 
 class WelcomeFragment : Fragment() {
 
-
+    lateinit var binding: FragmentWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        binding.welcomeButton.setOnClickListener {
+            findNavController().navigate(R.id.welcomeToSignup)
+        }
+
+        return binding.root
     }
 
 
