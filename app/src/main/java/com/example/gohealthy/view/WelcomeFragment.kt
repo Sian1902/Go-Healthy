4646366d4c8e35b1283ea07855bcca6ba9e97333
Ahmed Haitham <haithamelsersy5060@gmail.com>
2024-09-23 13:52:12 +0300

    tried accessing the steps counter it's currently working but still not staple

--- app/src/main/java/com/example/gohealthy/view/WelcomeFragment.kt
+++ app/src/main/java/com/example/gohealthy/view/WelcomeFragment.kt
@@ -1,11 +1,18 @@
 package com.example.gohealthy.view
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.gohealthy.Exercise.Exercise
 import com.example.gohealthy.Exercise.ExerciseData
 import com.example.gohealthy.NutritionixAPI.NutritionixQuery
@@ -17,13 +24,17 @@ import retrofit2.Callback
 import retrofit2.Response
 
 
-class WelcomeFragment : Fragment() {
+class WelcomeFragment : Fragment(), SensorEventListener {
 
     lateinit var binding: FragmentWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
+    private var sensorManager:SensorManager?=null
+    private var running= false
+    private var totalSteps=0f;
+    private var previousTotalSteps = 0f
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,13 +43,47 @@ class WelcomeFragment : Fragment() {
 
         binding.welcomeButton.setOnClickListener {
            // findNavController().navigate(R.id.welcomeToSignup)
-         nutritionCall()
-            exerciseCall()
+           // nutritionCall()
+           // exerciseCall()
+            loadData()
+            restSteps()
+            sensorManager = requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
 
         }
 
         return binding.root
     }
+
+    override fun onResume() {
+        super.onResume()
+        running=true
+        val stepSensor= sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        if(stepSensor==null){
+            Toast.makeText(requireContext(),"No sensor detected on this device", Toast.LENGTH_LONG).show()
+        }
+        else{
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+    override fun onPause() {
+        super.onPause()
+        sensorManager?.unregisterListener(this)
+    }
+
+    private fun loadData() {
+        val sharedPreferences = requireContext().getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences.getFloat("key1", 0f)
+        previousTotalSteps = savedNumber
+    }
+
+    private fun restSteps() {
+        val sharedPreferences = requireContext().getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putFloat("key1", previousTotalSteps)
+        editor.apply()
+    }
+
     fun nutritionCall(){
         val nutritionQuery = NutritionixQuery("100 gram meat")
         val call = RetrofitClient.instance.getNutritionData(nutritionQuery)
@@ -94,6 +139,20 @@ class WelcomeFragment : Fragment() {
                 t.printStackTrace()
             }
         })
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+      /*  var stepsTaken=binding.stepsText*/
+        if(running){
+            totalSteps=event!!.values[0]
+            val currentSteps= totalSteps.toInt()-previousTotalSteps.toInt()
+            binding.stepsText.text=("$currentSteps")
+
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
     }
 
 }
