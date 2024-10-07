548529ef4957a1a52e61c9a9859e9b00207cc502
Eyaaddd7 <eyadyehia710@gmail.com>
2024-10-07 23:05:02 +0300

    Home Page Done with Circular Progress Bar (Backend of Circular bar finished)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation ("com.mikhaellopez:circularprogressbar:3.1.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.android.material:material:1.3.0")
--- app/src/main/java/com/example/gohealthy/view/HomePageFragment.kt
+++ app/src/main/java/com/example/gohealthy/view/HomePageFragment.kt
@@ -1,23 +1,59 @@
 package com.example.gohealthy.view
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.gohealthy.R
-
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 class HomePageFragment : Fragment() {
-
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false)
+        val view = inflater.inflate(R.layout.fragment_home_page, container, false)
+
+        // Access the CircularProgressBar using the inflated view
+        val circularProgressBar = view.findViewById<CircularProgressBar>(R.id.circularProgressBar)
+
+        circularProgressBar.apply {
+            // Set Progress
+            progress = 0f
+            // or with animation
+            setProgressWithAnimation(200f, 10000) // =1s // Awel parameter bey2ol eno hayewsal l 200f b3d 10 seconds
+
+            // Set Progress Max
+            progressMax = 200f
+
+            // Set ProgressBar Color
+            progressBarColor = Color.rgb(230,97,4)
+            // or with gradient
+//            progressBarColorStart = Color.RED
+//            progressBarColorEnd = Color.rgb(230,97,4)
+//            progressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+
+            // Set background ProgressBar Color
+            backgroundProgressBarColor = Color.rgb(230,97,4)
+            // or with gradient
+//            backgroundProgressBarColorStart = Color.WHITE
+//            backgroundProgressBarColorEnd = Color.RED
+            backgroundProgressBarColorDirection = CircularProgressBar.GradientDirection.RIGHT_TO_LEFT
+
+            // Set Width
+            progressBarWidth = 7f // in DP
+            backgroundProgressBarWidth = 3f // in DP
+
+            // Other
+            roundBorder = true
+            startAngle = 0f
+            progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+        }
+
+        return view // Return the inflated view
     }
 }
